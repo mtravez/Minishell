6f0c6888361eedf7f8bd5d467036d330b252c2e1
Maria Jose Travez <mtravez@student.42heilbronn.de>
2023-06-30 13:57:30 +0200

    cd is now dependant on the environment

--- builtins/builtin_cd.c
+++ builtins/builtin_cd.c
@@ -6,7 +6,7 @@
 /*   By: mtravez <mtravez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/01 17:19:43 by mtravez           #+#    #+#             */
-/*   Updated: 2023/06/28 17:05:48 by mtravez          ###   ########.fr       */
+/*   Updated: 2023/06/30 13:55:38 by mtravez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,19 +27,41 @@ int	directory_exists(char *dir)
 /*This function replaces the '~' at the beginning of a directory
 path with the HOME environmental variable.
 @param dir string with a path to a directory.*/
-char	*expand_directory(char *dir)
+char	*expand_directory(char *dir, t_envar **env)
 {
 	char	*new_dir;
+	char	*home_str;
+	t_envar	*home;
 
+	home = get_var(env, "HOME");
+	home_str = ft_strdup(".");
+	if (home && home->content)
+	{
+		free(home_str);
+		home_str = ft_strdup(home->content);
+	}
 	if (dir && dir[0] == '~')
 	{
-		new_dir = ft_strjoin_gnl(
-				ft_strdup(getenv("HOME")), ft_strdup(&(dir[1])));
+		new_dir = ft_strjoin_gnl(home_str, ft_strdup(&(dir[1])));
 		return (new_dir);
 	}
 	if (!dir)
-		return (ft_strdup(getenv("HOME")));
+		return (home_str);
+	free(home_str);
 	return (ft_strdup(dir));
+}
+
+char	*get_expanded_dir(char *arg, t_envar **env)
+{
+	char	*dir;
+
+	dir = expand_directory(arg, env);
+	if (ft_strncmp(dir, "", 1) == 0)
+	{
+		free(dir);
+		dir = ft_strdup(".");
+	}
+	return (dir);
 }
 
 /*This is the built in cd command for minishell.
@@ -50,7 +72,7 @@ int	ft_cd(t_exec *exec)
 	int		exit_nr;
 	char	*dir;
 
-	dir = expand_directory(exec->argv[1]);
+	dir = get_expanded_dir(exec->argv[1], exec->env);
 	if (access(dir, F_OK) != 0)
 	{
 		ft_putstr_fd("minishell: cd: ", exec->out_fd);
